ec365ac01515e0675be198a0bbf80aa3c1a4bda5
jyotimishra1 <jyotimishra.jec@gmail.com>
2020-06-28 12:21:25 +0530

    Initial commit

---
+++ src/main/java/com/example/SpringSecurityLDAP/HomeResource.java
@@ -0,0 +1,16 @@
+package com.example.SpringSecurityLDAP;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+
+    @GetMapping("/")
+    public String index()
+    {
+        return "home page";
+    }
+
+}
+
